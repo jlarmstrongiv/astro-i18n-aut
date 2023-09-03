a7502389dcbd1530843be8417bf4438d374a1fce
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2023-09-02 20:23:16 -0600

    hotfix: middleware respect BASE_URL

--- src/edge-runtime/middleware.ts
+++ src/edge-runtime/middleware.ts
@@ -1,6 +1,10 @@
 import type { ValidRedirectStatus } from "astro";
 import { defineMiddleware } from "astro/middleware";
-import { defaultLocale, redirectDefaultLocale } from "./config";
+import {
+  defaultLocale,
+  redirectDefaultLocale,
+  BASE_URL as baseUrl,
+} from "./config";
 
 export const i18nMiddleware = defineMiddleware((context, next) => {
   if (redirectDefaultLocale === false) {
@@ -12,18 +16,30 @@ export const i18nMiddleware = defineMiddleware((context, next) => {
     status = redirectDefaultLocale;
   }
 
-  const requestUrlPathname = new URL(context.request.url).pathname;
+  const pathName = new URL(context.request.url).pathname;
+
+  let pathNameWithoutBaseUrl: string;
+  if (baseUrl === "/") {
+    // there is no baseUrl set
+    pathNameWithoutBaseUrl = pathName;
+  } else {
+    // remove trailingSlash from baseUrl if it exists
+    let baseUrlWithoutTrailingSlash = baseUrl.endsWith("/")
+      ? baseUrl.slice(0, -1)
+      : baseUrl;
+
+    // remove baseUrlWithoutTrailingSlash from pathNameWithoutBaseUrl
+    pathNameWithoutBaseUrl = pathName.replace(baseUrlWithoutTrailingSlash, "");
+  }
+
   // avoid catching urls that start with "/en" like "/enigma"
-  if (requestUrlPathname === `/${defaultLocale}`) {
-    return context.redirect(
-      requestUrlPathname.replace(`/${defaultLocale}`, "/"),
-      status
-    );
+  if (pathNameWithoutBaseUrl === "/" + defaultLocale) {
+    return context.redirect(pathName.replace("/" + defaultLocale, "/"), status);
   }
   // catch all "/en/**/*" urls
-  if (requestUrlPathname.startsWith(`/${defaultLocale}/`)) {
+  if (pathNameWithoutBaseUrl.startsWith("/" + defaultLocale + "/")) {
     return context.redirect(
-      requestUrlPathname.replace(`/${defaultLocale}/`, "/"),
+      pathName.replace("/" + defaultLocale + "/", "/"),
       status
     );
   }
