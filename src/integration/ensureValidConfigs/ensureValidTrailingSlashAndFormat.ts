38db30a811d2ab8b3f158074f9bd3982e689649e
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2023-09-13 11:12:14 -0600

    refactor: type safe updateConfig with defineConfig

--- src/integration/ensureValidConfigs/ensureValidTrailingSlashAndFormat.ts
+++ src/integration/ensureValidConfigs/ensureValidTrailingSlashAndFormat.ts
@@ -1,4 +1,5 @@
 import type { AstroConfig, AstroIntegrationLogger } from "astro";
+import { defineConfig } from "astro/config";
 import type { UpdateConfig } from "./UpdateConfig";
 
 export function ensureValidTrailingSlashAndFormat(
@@ -17,30 +18,36 @@ export function ensureValidTrailingSlashAndFormat(
       `config.trailingSlash = "never" && config.build.format = "file"`
     );
     logger.warn(`setting config.trailingSlash = "${config.trailingSlash}"`);
-    updateConfig({
-      trailingSlash: config.build.format === "directory" ? "always" : "never",
-    });
+    updateConfig(
+      defineConfig({
+        trailingSlash: config.build.format === "directory" ? "always" : "never",
+      })
+    );
   }
   if (config.trailingSlash === "always" && config.build.format === "file") {
     logger.warn(
       `config.trailingSlash = "always" must always be used with config.build.format = "directory"`
     );
     logger.warn(`setting config.build.format = "directory"`);
-    updateConfig({
-      build: {
-        format: "directory",
-      },
-    });
+    updateConfig(
+      defineConfig({
+        build: {
+          format: "directory",
+        },
+      })
+    );
   }
   if (config.trailingSlash === "never" && config.build.format === "directory") {
     logger.warn(
       `config.trailingSlash = "never" must always be used with config.build.format = "file"`
     );
     logger.warn(`setting config.build.format = "file"`);
-    updateConfig({
-      build: {
-        format: "file",
-      },
-    });
+    updateConfig(
+      defineConfig({
+        build: {
+          format: "file",
+        },
+      })
+    );
   }
 }
